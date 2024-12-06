c733624d845c0345a00b77862a40c1ebb128356f
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-12-06 02:48:56 +0200

    Added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+// Navbar for navigation
+function AppNavbar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Food Pulse</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <Nav.Link href="/Users">Users</Nav.Link>
+            <Nav.Link href="/Recipes">Recipes</Nav.Link>
+            <Nav.Link href="/Categories">Category</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default AppNavbar;
