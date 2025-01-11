a082864fce8f7f0b91006e591cb2845a7f55d6c1
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2025-01-11 18:05:15 +0200

    removed dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/LoginPage';
-import Dashboard from './Pages/Dashboard';
-import AddRecipe from './Pages/Feed/AddRecipe'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddUser from './Pages/Users/AddUser';
@@ -24,10 +22,9 @@ function App() {
           <Routes>
               {/* Define routes */}
               <Route path="/Login" element={<Login />} /> 
-              <Route path="/dashboard" element={<Dashboard />} /> 
 
               <Route path='/' Component={Feed} />
-              <Route path="/Recipe/Add" Component={AddRecipe} />
+              {/* <Route path="/Recipe/Add" Component={AddRecipe} /> */}
 
               <Route path="/Register" Component={AddUser} />
               <Route path="/Users" Component={UsersList} />
